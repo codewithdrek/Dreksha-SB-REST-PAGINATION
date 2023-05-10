478bdc74fac1f1130e8f395eaa177a4d7044001c
Admin <you@example.com>
2023-05-10 10:45:16 +0530

    first commit

---
+++ src/main/java/com/luv2code/springboot/curddemo/dao/EmployeeReporisiotry.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.curddemo.dao;
+
+import com.luv2code.springboot.curddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeReporisiotry extends JpaRepository<Employee,Integer> {
+//no need to write any code
+
+
+    //http://localhost:8080/magic-api/employee?page=1@size=3
+    //http://localhost:8080/magic-api/employee?sort=lastName ,Desc
+    //http://localhost:8080/magic-api/employee?sort=firstName ,Asec
+}
